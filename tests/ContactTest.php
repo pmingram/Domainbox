97afa29c287666216c71e619003d0571374d4822
Made I.T <info@madeit.be>
2020-10-02 00:22:09 +0200

    Update ContactTest.php

--- tests/ContactTest.php
+++ tests/ContactTest.php
@@ -1,12 +1,13 @@
 <?php
 
 use MadeITBelgium\Domainbox\Domainbox;
+use PHPUnit\Framework\TestCase;
 
-class ContactTest extends \PHPUnit_Framework_TestCase
+class ContactTest extends TestCase
 {
     private $wsdl = 'tests/domainbox.wsdl';
 
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
     }
