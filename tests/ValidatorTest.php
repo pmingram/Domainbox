e851932de2b4f9f50c225028e21dc90dcba1326d
Made I.T <info@madeit.be>
2020-10-02 00:31:11 +0200

    Update ValidatorTest.php

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -50,6 +50,8 @@ class ValidatorTest extends TestCase
         $translator->shouldReceive('get')->once()->with('validation.domainname')->andReturn('validation.domainname');
         $translator->shouldReceive('get')->once()->with('validation.attributes')->andReturn('validation.attributes');
         $translator->shouldReceive('get')->once()->with('validation.attributes.foo')->andReturn('validation.attributes.foo');
+        $translator->shouldReceive('get')->once()->with('validation.values.foo.madeit.belgium')->andReturn('validation.values.foo.madeit.belgium');
+        
         $factory = new Factory($translator, $container);
         $factory->extend('domainname', 'MadeITBelgium\Domainbox\Validation\ValidatorExtensions@validateDomainname', ':attribute must be a valid Domainname');
         $validator = $factory->make(['foo' => 'madeit.belgium'], ['foo' => 'domainname']);
